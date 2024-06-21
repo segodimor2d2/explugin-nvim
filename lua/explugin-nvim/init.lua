04d93e75c0c3cbfd19e0d1004e773ec643e51f50
segodimo <segodimo@gmail.com>
2024-06-21 17:22:35 -0300

    cap02_01

--- lua/explugin-nvim/init.lua
+++ lua/explugin-nvim/init.lua
@@ -1,13 +1,18 @@
 local M = {}
 
 M.fntest = function()
-	print("test my first plugin !!")
+  -- print("test my first plugin !!!")
+  local getBuffer = vim.api.nvim_get_current_buf()
+
+	vim.api.nvim_buf_set_lines(getBuffer , 1, 5, false,
+    { "aqui um novo texto"}) -- incerir na ultima linha
 end
 
 M.setup = function()
-	vim.keymap.set("n", "<leader>pk", function()
+  vim.keymap.set("n", "<leader>pk", function()
 		M.fntest()
 	end, { desc = "EX-Plugin-Nvim" })
 end
 
 return M
+
