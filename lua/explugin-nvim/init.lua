4304b9f8aad71baa49abe7c0e46a309d1a4d014a
segodimo <segodimo@gmail.com>
2024-06-21 22:15:00 -0300

    cap05

--- lua/explugin-nvim/init.lua
+++ lua/explugin-nvim/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.fntest = function()
-	-- print("test my first plugin !!!")
 	local getBuffer = vim.api.nvim_get_current_buf()
 
 	local _, srow, scol = unpack(vim.fn.getpos("v"))
@@ -20,11 +19,6 @@ M.fntest = function()
 		lines = vim.api.nvim_buf_get_lines(0, lini - 1, lfin, true)
 	end
 
-	-- Concatenate lines into a single string separated by '|'
-	-- local result = table.concat(lines, ",")
-	-- print(result)
-	--
-
 	local outTxt = "\n"
 	outTxt = outTxt .. "Olá "
 	outTxt = outTxt .. lines[1]
@@ -37,15 +31,8 @@ M.fntest = function()
 	outTxt = outTxt .. "\n\n"
 	outTxt = outTxt .. "Atte. Sebastian"
 
-	-- Divide a string outTxt em linhas
-	-- local outLines = {}
-	-- for line in outTxt:gmatch("[\r\n]+") do
-	-- 	table.insert(outLines, line)
-	-- end
-
 	local outLines = vim.split(outTxt, "\n", true)
 
-	-- Inserir as linhas no buffer
 	vim.api.nvim_buf_set_lines(getBuffer, lfin, lfin, false, outLines)
 end
 
